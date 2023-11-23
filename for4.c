53b4427200d18864d1ad945e32838f6e0ab907ad
Keyur Lakhani <lkeyur20@gmail.com>
2023-11-23 22:24:11 +0530

    prtrn

--- for4.c
+++ for4.c
@@ -5,19 +5,20 @@
 1 2 3 4 5    */
 #include <stdio.h>
 void main()
-{  int j,i;
-i=1;
-j=1;
-    for(i=1; i<=5; i++)
+{
+    int j, i;
+    i = 1;
+    j = 1;
+    for (i = 1; i <= 5; i++)
     {
         printf("-");
     }
-    for( i=1;i<=5;i++)
+    for (i = 1; i <= 5; i++)
     {
-        printf("%d ",j);
+        printf("%d ", j);
     }
     printf("\n");
 
-    for(i=1;i<=5;i++)
-    printf("-");
+    for (i = 1; i <= 5; i++)
+        printf("-");
 }
