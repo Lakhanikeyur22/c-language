7f796d9093350e6a720ffc98c0a160822f970e0f
Keyur Lakhani <lkeyur20@gmail.com>
2023-12-15 11:01:12 +0530

    petrn

--- fstar3.c
+++ fstar3.c
@@ -1,10 +1,18 @@
+/*
+ * * * * *
+ *     *
+ *   *
+ * *
+ *
+ */
 #include <stdio.h>
 void main()
 {
     int i, j, k, number;
     printf(" enter line of number := ");
     scanf("%d", &number);
-    for (j = 1, k = number; j <= number; j++, k--)
+    k = number;
+    for (j = 1; j <= number; j++, k--)
     {
         for (i = 1; i <= j; i++)
         {
---
+++ string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    char name[2];
+    printf(" ANTER YOUR NAME : ");
+    scanf("%s", &name);
+
+    printf(" name  is %s", name);
+}
